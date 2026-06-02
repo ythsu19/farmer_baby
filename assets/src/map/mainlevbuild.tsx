da87af10480d34eeec9691bd8cd8f97aee5e8b41
ythsu <ythsu19@gmail.com>
2026-06-02 18:11:52 +0800

    map

---
+++ assets/src/map/mainlevbuild.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="mainlevbuild" tilewidth="32" tileheight="32" tilecount="2000" columns="50">
+ <image source="mainlevbuild.png" width="1600" height="1280"/>
+</tileset>
